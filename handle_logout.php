241b1d937a979c80455c335a39afea21b1b76ff1
John Turknett <xboy2k11@gmail.com>
2014-02-10 22:02:53 -0800

    handle_logout.php: Send special URL to mobile app to handle native logout

--- handle_logout.php
+++ handle_logout.php
@@ -8,7 +8,9 @@ $sc = $_COOKIE['griddle_session'];
 
 $res = mysql_query("UPDATE session SET din=0 WHERE sess_cookie='$sc'");
 
-
-header( "Location: http://$baseSRV" ); 
-
+if(!$MOBSERV) {
+   header( "Location: http://$baseSRV" ); 
+} else {
+   header( "Location: http://$baseSRV/loggedout" );
+}
 ?>
