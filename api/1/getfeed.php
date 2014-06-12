b93efc51368edc3ccac92f2eb45b3bb992e1a818
John Turknett <xboy2k11@gmail.com>
2014-06-11 19:43:33 -0700

    getfeed.php: Add bbid and gid code

--- api/1/getfeed.php
+++ api/1/getfeed.php
@@ -25,45 +25,56 @@ if(apiAuth($user, $pass) < 1) {
 
 $uid = getUser($user);
 
-//$ORDER_BY = "griddle_bb.din";
-$ORDER_BY = "RAND()";
-
-if(!$bbid) {
-   $SQL = "SELECT griddle_bb.bbid, griddle_bb.uid, relations.uid, users.name, griddle_bb.gid FROM relations, users, griddle_bb WHERE griddle_bb.status=1 AND relations.friend=2 AND relations.target=$uid AND users.uid=relations.uid AND griddle_bb.uid=users.uid ORDER BY $ORDER_BY DESC LIMIT $count";
-}
-
-$res = mysql_query("$SQL");
-
 $JSON = "{ \"return\": \"SUCCESS\", \"posts\": [ ";
 
-while($row = mysql_fetch_array($res)) {
+if($bbid) { 
+   $JSON .= getBBIDFeed($bbid, $uid);
+}
 
-   $bbid  = $row{'bbid'};
-   $bbi   = getGriddleInfo($bbid);
-   $PLIST = explode(",", $bbi{'ppid'});
-   
-   $pid   = $PLIST[2];
-   $gid   = $row{'gid'};
+if($gid) {
+   $JSON .= getGIDFeed($gid, $uid);
+}
 
-   $pi    = getPostInfo($pid);
-   $puid  = $pi{'uid'};
-   $img   = shardImg($pi{'images'}) . "/mid_images/" . $pi{'images'};
-   $timg  = shardImg($pi{'images'}) . "/thumb_images/" . $pi{'images'};
-   
-   $comms = $pi{'comments'};
-   $hots  = $pi{'hots'};
-   $ui    = getUserInfo($puid);
-   $n     = $ui{'name'};
-   $un    = $ui{'username'};
-   $pimg  = "http://www.griddle.com/thumb_profiles/$un";
-   
-   if(didHotG($uid, $bbid)) { $didhot = "1"; } else { $didhot="0"; }
-   if(didCommG($uid, $bbid)) { $didcom = "1"; } else { $didcom="0"; }
+if(!$bbid && !$gid) {
+   $JSON .= getRandomFeed($uid);
+} 
 
-   $more    = "1";
-   $when    = secondsToTime(time() - $pi{'din'});
+$JSON .= " ] }";
+print "$JSON";
+
+
+function getGIDFeed($gid, $uid) {
+    $SQL = "SELECT bbid FROM griddle_bb WHERE gid=$gid AND status=1";
+    $res = mysql_query($SQL);
+    while($row = mysql_fetch_array($res)) {	
+    $bbid  = $row{'bbid'};
+     $bbi   = getGriddleInfo($bbid);
+     $PLIST = explode(",", $bbi{'ppid'});
    
-   $JSON .= "{ \"n\": \"$n\",
+     $maxr  = count($PLIST);
+     $picr  = rand(0, $maxr);
+     $pid   = $PLIST[$picr];
+     $gid   = $row{'gid'};
+  
+     $pi    = getPostInfo($pid);
+     $puid  = $pi{'uid'};
+     $img   = shardImg($pi{'images'}) . "/mid_images/" . $pi{'images'};
+     $timg  = shardImg($pi{'images'}) . "/thumb_images/" . $pi{'images'};
+   
+     $comms = $pi{'comments'};
+     $hots  = $pi{'hots'};
+     $ui    = getUserInfo($puid);
+     $n     = $ui{'name'};
+     $un    = $ui{'username'};
+     $pimg  = "http://www.griddle.com/thumb_profiles/$un";
+   
+     if(didHotG($uid, $bbid)) { $didhot = "1"; } else { $didhot="0"; }
+     if(didCommG($uid, $bbid)) { $didcom = "1"; } else { $didcom="0"; }
+
+     $more    = "1";
+     $when    = secondsToTime(time() - $pi{'din'});
+   
+     $JSON .= "{ \"n\": \"$n\",
                \"un\": \"$un\",
                \"img\": \"$img\",
                \"timg\": \"$timg\",
@@ -76,18 +87,122 @@ while($row = mysql_fetch_array($res)) {
                \"pid\": \"$pid\",
                \"bbid\": \"$bbid\",
                \"gid\": \"$gid\" },\n";
-
+  
+  } 
    
+  $JSON = rtrim($JSON, ",\n"); 
+  
+  return $JSON;
+
+}
+    
+
+
+
+function getBBIDFeed($bbid, $uid) {
+     
+     $bbi   = getGriddleInfo($bbid);
+     $PLIST = explode(",", $bbi{'ppid'});
+     foreach ($PLIST as $pid) {
+        $pi    = getPostInfo($pid);
+        $puid  = $pi{'uid'};
+        $img   = shardImg($pi{'images'}) . "/mid_images/" . $pi{'images'};
+        $timg  = shardImg($pi{'images'}) . "/thumb_images/" . $pi{'images'};
+   
+        $comms = $pi{'comments'};
+        $hots  = $pi{'hots'};
+        $ui    = getUserInfo($puid);
+        $n     = $ui{'name'};
+        $un    = $ui{'username'};
+        $pimg  = "http://www.griddle.com/thumb_profiles/$un";
+   
+        if(didHotG($uid, $bbid)) { $didhot = "1"; } else { $didhot="0"; }
+        if(didCommG($uid, $bbid)) { $didcom = "1"; } else { $didcom="0"; }
+
+        $more    = "1";
+        $when    = secondsToTime(time() - $pi{'din'});
+   
+        $JSON .= "{ \"n\": \"$n\",
+               \"un\": \"$un\",
+               \"img\": \"$img\",
+               \"timg\": \"$timg\",
+               \"pimg\": \"$pimg\",
+               \"comms\": \"$comms\",
+               \"hots\": \"$hots\",
+               \"didhot\": \"$didhot\",
+               \"didcom\": \"$didcom\",
+               \"more\": \"$more\",
+               \"pid\": \"$pid\",
+               \"bbid\": \"$bbid\",
+               \"gid\": \"$gid\" },\n";
+  
+     }          
+     $JSON = rtrim($JSON, ",\n"); 
+  
+     return $JSON;
 
 }
 
-$JSON = rtrim($JSON, ",\n");
-
-$JSON .= " ] }";
 
 
+function getRandomFeed($uid) {
 
-print "$JSON";
+   $ORDER_BY = "RAND()";
+   $SQL = "SELECT griddle_bb.bbid, griddle_bb.uid, relations.uid, users.name, griddle_bb.gid FROM relations, users, griddle_bb WHERE griddle_bb.status=1 AND relations.friend=2 AND relations.target=$uid AND users.uid=relations.uid AND griddle_bb.uid=users.uid ORDER BY $ORDER_BY DESC LIMIT $count";
+   $res = mysql_query($SQL);
+   
+   while($row = mysql_fetch_array($res)) {
+
+     $bbid  = $row{'bbid'};
+     $bbi   = getGriddleInfo($bbid);
+     $PLIST = explode(",", $bbi{'ppid'});
+   
+     $maxr  = count($PLIST);
+     $picr  = rand(0, $maxr);
+     $pid   = $PLIST[$picr];
+     $gid   = $row{'gid'};
+  
+     $pi    = getPostInfo($pid);
+     $puid  = $pi{'uid'};
+     $img   = shardImg($pi{'images'}) . "/mid_images/" . $pi{'images'};
+     $timg  = shardImg($pi{'images'}) . "/thumb_images/" . $pi{'images'};
+   
+     $comms = $pi{'comments'};
+     $hots  = $pi{'hots'};
+     $ui    = getUserInfo($puid);
+     $n     = $ui{'name'};
+     $un    = $ui{'username'};
+     $pimg  = "http://www.griddle.com/thumb_profiles/$un";
+   
+     if(didHotG($uid, $bbid)) { $didhot = "1"; } else { $didhot="0"; }
+     if(didCommG($uid, $bbid)) { $didcom = "1"; } else { $didcom="0"; }
+
+     $more    = "1";
+     $when    = secondsToTime(time() - $pi{'din'});
+   
+     $JSON .= "{ \"n\": \"$n\",
+               \"un\": \"$un\",
+               \"img\": \"$img\",
+               \"timg\": \"$timg\",
+               \"pimg\": \"$pimg\",
+               \"comms\": \"$comms\",
+               \"hots\": \"$hots\",
+               \"didhot\": \"$didhot\",
+               \"didcom\": \"$didcom\",
+               \"more\": \"$more\",
+               \"pid\": \"$pid\",
+               \"bbid\": \"$bbid\",
+               \"gid\": \"$gid\" },\n";
+  
+  } 
+   
+  $JSON = rtrim($JSON, ",\n"); 
+  
+  return $JSON;
+
+}
+
+
 
 
 
