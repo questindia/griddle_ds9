910af3872c0d174c158b5527cf761f40a2c88e68
John Turknett <xboy2k11@gmail.com>
2014-06-16 21:00:08 -0700

    dohot.php: Once more, with feeling

--- api/1/dohot.php
+++ api/1/dohot.php
@@ -13,6 +13,57 @@ if(apiAuth($user, $pass) < 1) {
      exit;
 }
 
+if($bbid) { doBBID($user, $bbid); } else { doPID($user, $pid); }
+
+
+
+
+function doPID($user, $pid) {
+$vote = "up";
+
+$uid = getUser($user);
+
+$res = mysql_query("SELECT uid, pid FROM hots WHERE uid=$uid AND pid=$pid");
+$row = mysql_fetch_array($res);
+$there = $row{'uid'};
+if($there) {
+    $vote = "down";
+    $res = mysql_query("DELETE FROM hots WHERE uid=$uid AND pid=$pid");
+    $new = "no";
+} 
+
+$res = mysql_query("SELECT hots, uid FROM posts WHERE pid=$pid");
+$row = mysql_fetch_array($res);
+$hots = $row{'hots'};
+$puid = $row{'uid'};
+if($vote == "up") {
+   $hots = $hots + 1;
+   $res = mysql_query("UPDATE users SET pounds = pounds + 1 WHERE uid=$puid");
+} elseif ($vote == "down") {
+   $hots = $hots - 1;
+}
+
+   
+$res = mysql_query("UPDATE posts SET hots=$hots WHERE pid=$pid");
+   
+$din = time();
+
+if($new!="no") {
+    $res = mysql_query("INSERT INTO hots VALUES($uid, $pid, 0, $din)");
+}
+
+print "{ \"return\": \"SUCCESS\", \"hots\":$hots }";
+
+}
+
+
+
+
+
+function doBBID($user, $bbid) {
+
+$vote = "up";
+
 $uid = getUser($user);
 
 $res = mysql_query("SELECT uid, bbid FROM hots_bb WHERE uid=$uid AND bbid=$bbid");
@@ -46,7 +97,7 @@ if($new!="no") {
 
 print "{ \"return\": \"SUCCESS\", \"hots\":$hots }";
 
-
+}
 
 
 
